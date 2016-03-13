4b797ffcef9eb447628a1058e7ea51472761b2b7
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2016-03-13 15:28:19 -0400

    Added Card, List, and TimeEntry. Some new Trello API stuff.

--- trello.py
+++ trello.py
@@ -12,6 +12,7 @@ class TrelloAPI(object):
         url = "https://trello.com/1/" + url_path
 
         response = requests.get(url, params=params)
+        print response.request.url
         if response.status_code != 200:
             request = response.request
             raise RuntimeError("{} request {} returned {}".format(request.method,
@@ -32,6 +33,21 @@ class TrelloAPI(object):
         return self.get("organizations/{}/boards".format(org_name),
                         params={'filter': 'open'})
 
+    def get_board_ids_by_organization(self, org_name):
+        return [ json['id'] for json in self.get("organizations/{}/boards".format(org_name),
+                                                 params={'filter': 'open',
+                                                         'fields': 'id'}) ]
+
     def get_members_by_organization(self, org_name):
         return self.get("organizations/{}".format(org_name),
                         params={'members': 'all'})
+
+    def get_board_details_by_board_id(self, board_id):
+        return self.get("boards/{}".format(board_id),
+                        params={'organization': 'true',
+                                'cards': 'all',
+                                'lists': 'all',
+                                'actions': 'commentCard',
+                                'memberships_member': 'true',
+                                'memberships': 'all',
+                                'memberships_member_fields': 'fullName,username,memberType,initials'})
